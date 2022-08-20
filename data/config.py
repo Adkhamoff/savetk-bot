0f84fb5bdec3a51734fce7b207e3dc758afcdfb1
Adkhamoff <90890798+Adkhamoff@users.noreply.github.com>
2022-08-20 17:22:25 +0500

    firist commit

--- data/config.py
+++ data/config.py
@@ -5,6 +5,6 @@ env = Env()
 env.read_env()
 
 # .env fayl ichidan quyidagilarni o'qiymiz
-BOT_TOKEN ="5549332460:AAHBHfnXv2GmeAgY5_aS15cIkrqouQRDOfo"  # Bot toekn
-ADMINS =[324271904]  # adminlar ro'yxati
+BOT_TOKEN =""  # Bot toekn
+ADMINS =[]  # adminlar ro'yxati
 #IP = env.str("ip")  # Xosting ip manzili
